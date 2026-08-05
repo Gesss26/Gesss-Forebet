af568c6aa56b00814db2e77045dcc37188b7d0f7
Gesss26 <gesualdo.sangiorgi@gmail.com>
2026-08-05 09:21:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,19 +9,20 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
     self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-    e.waitUntil(caches.keys().then(keys => 
-        Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ));
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
     self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(cached => cached || fetch(e.request))
-    );
+    // Non cachare il file XLSX da GitHub (sempre fresco)
+    if (e.request.url.includes('raw.githubusercontent.com')) {
+        e.respondWith(fetch(e.request));
+        return;
+    }
+    e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
 });
